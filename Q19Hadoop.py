8493f0c4f12576ef4a993407570940b7e1a33218
Anant <x21188947@student.ncirl.ie>
2023-08-08 15:15:36 +0000

    final

--- Q19Hadoop.py
+++ Q19Hadoop.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python
+# Import necessary modules
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import datetime
 
 #19. On which date was the latest fatal app error where the message contains "Error reading message prefix"?
+# Define an MRJob class to find the latest fatal app error with a specific message content.
+
 
 class MRJobLatestFatalError(MRJob):
+    
+    # Mapper function: Extracts relevant information from log lines and emits timestamps for fatal errors with specified message content.
     def latest_fatal_error_mapper(self, _, line):
         fields = line.split()
         if len(fields) >= 8:
@@ -16,7 +21,8 @@ class MRJobLatestFatalError(MRJob):
                 date = datetime.datetime.strptime(date_str, "%Y.%m.%d")
                 timestamp = int(date.timestamp())
                 yield None, timestamp
-
+                
+    # Combiner function: Identifies the latest timestamp among the emitted timestamps.
     def latest_fatal_error_combiner(self, _, timestamps):
         latest_timestamp = None
         for timestamp_str in timestamps:
@@ -27,6 +33,7 @@ class MRJobLatestFatalError(MRJob):
                 latest_timestamp = timestamp
         yield None, latest_timestamp
 
+    # Reducer function: Determines the latest fatal error timestamp and yields the corresponding date.
     def latest_fatal_error_reducer(self, _, timestamps):
         latest_timestamp = None
         for timestamp_str in timestamps:
--- Q19SparkSQL.py
+++ Q19SparkSQL.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, LongType, DateType
 from pyspark.sql.functions import col
